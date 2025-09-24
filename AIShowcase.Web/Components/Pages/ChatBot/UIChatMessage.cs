d3a153f5223c8c329e77da7d0dfd1266d87ed2c5
Ed Charbeneau <ed.charbeneau@gmail.com>
2025-09-24 16:00:50 -0400

    Update chat components and styles for Kendo UI
    - Updated Kendo UI stylesheet links in `App.razor` to version 12.0.1.
    - Restructured chat interface in `ChatWindow.razor` using `TelerikChat` and introduced `UIChatMessage` class for message handling.
    - Modified layout in `AboutSpeechToText.razor` and replaced `SpeechToTextButton` with `TelerikSpeechToTextButton`.
    - Removed avatar styles in `style.css` while retaining auto-scroll behavior.
    - Added `UIChatMessage.cs` to define a new class for chat messages with relevant properties and static methods for message creation.

---
+++ AIShowcase.Web/Components/Pages/ChatBot/UIChatMessage.cs
@@ -0,0 +1,37 @@
+﻿namespace AIShowcase.WebApp.Components.Pages.ChatBot;
+public class UIChatMessage
+{
+	public string? Id { get; set; } = Guid.NewGuid().ToString();
+	public string? AuthorId { get; set; }
+
+	public string? AuthorName { get; set; }
+
+	public string? AuthorImageUrl { get; set; }
+
+	public string? Content { get; set; }
+
+	public string Status { get; set; } = "Sent";
+
+	public bool IsDeleted { get; set; }
+
+	public bool IsPinned { get; set; }
+
+	public DateTime Timestamp { get; set; } = DateTime.Now;
+
+	//public List<string> SuggestedActions { get; set; }
+
+	public static UIChatMessage AssistantMessage(string content) => new UIChatMessage()
+	{
+		AuthorId = "ai",
+		AuthorName = "AI Assistant",
+		AuthorImageUrl = "https://demos.telerik.com/blazor-ui/images/devcraft-ninja-small.svg",
+		Content = content,
+	};
+
+	public static UIChatMessage UserMessage(string content) => new UIChatMessage()
+	{
+		AuthorId = "user",
+		AuthorName = "Your Message",
+		Content = content,
+	};
+}
